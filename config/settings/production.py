277d42acedbca1061a6b6231c382018ccd8789d2
incognito <stha.binod1000@gmail.com>
2023-03-22 22:23:42 +0545

    F

--- config/settings/production.py
+++ config/settings/production.py
@@ -13,7 +13,7 @@ import datetime
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["ecom.com"])
+ALLOWED_HOSTS = ["*"]
 
 # DATABASES
 # ------------------------------------------------------------------------------
