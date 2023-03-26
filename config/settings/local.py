c446694b4141736a99335d488bbababb2a0d1d2f
incognito <stha.binod1000@gmail.com>
2023-03-26 16:43:57 +0545

    #Time 20min #comment TH

--- config/settings/local.py
+++ config/settings/local.py
@@ -50,7 +50,7 @@ DEBUG_TOOLBAR_CONFIG = {
 USE_DOCKER='yes'
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
 INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
-if env("USE_DOCKER", None) == "yes":
+if USE_DOCKER == "yes":
     import socket
 
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
