72c748835c13a95070a038fedea176d9c7c8f23f
incognito <stha.binod1000@gmail.com>
2023-03-26 16:42:46 +0545

    #Time 20min #comment TH

--- config/settings/local.py
+++ config/settings/local.py
@@ -47,6 +47,7 @@ DEBUG_TOOLBAR_CONFIG = {
     "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
     "SHOW_TEMPLATE_CONTEXT": True,
 }
+USE_DOCKER='yes'
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
 INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
 if env("USE_DOCKER", None) == "yes":
